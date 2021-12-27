0857d67c0b32d270ebc3f49153e65d9b685ba6de
Younsoo Tom Lim <tomandlim@gmail.com>
2021-12-28 00:26:02 +0900

    Added: basic example of server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,5 +1,29 @@
 const http = require('http');
 
-const server = http.createServer(() => {});
+const server = http.createServer((req, res) => {
+  // console.log(req.method);
+  const url = req.url;
 
-console.log('Express Tutorial');
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.write('<h1>home page</h1>');
+    res.end();
+  }
+  // about
+  else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.write('<h1>about page</h1>');
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { 'content-type': 'text/plain' });
+    res.write('<h1>page not found</h1>');
+    res.end();
+  }
+  // res.writeHead(200, { 'content-type': 'text/html' });
+});
+
+server.listen(5000, () => {
+  console.log('Server is listening on port 5000...');
+});
