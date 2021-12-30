e3bf1f0ff0b21518ea2d69d4e18bd9aab39a822a
Younsoo Tom Lim <tomandlim@gmail.com>
2021-12-30 20:47:38 +0900

    Added: basic route structure and getAllTasks

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,24 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+// middleware
+
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App');
+});
+
+// routes;
+app.use('/api/v1/tasks', tasks);
+
+// app.get('/api/v1/tasks')
+// app.post('/api/v1/tasks')
+// app.get('/api/v1/tasks/id:')
+// app.patch('/api/v1/tasks/id:')
+// app.delete('/api/v1/tasks/id:')
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
