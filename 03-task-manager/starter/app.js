e3d2d1b47d7d73033f78e04cf8d6588ff71f76a5
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-01 20:07:49 +0900

    Refactor: db connect

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,3 +1,5 @@
+const connectDB = require('./db/connect');
+
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
@@ -20,6 +22,15 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 5000;
 
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,2 +1,18 @@
+const mongoose = require('mongoose');
+
 const connectionString =
-  'mongodb+srv://yslim:<password>@nodeexpressproject.a6dqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+  'mongodb+srv://yslim:9dnjfxhRl@nodeexpressproject.a6dqt.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority';
+
+const connectDB = url => {
+  return mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('connected to the db...'))
+    .catch(err => console.log(err));
+};
+
+module.exports = connectDB;
