e0b8f41a8c73e8e4301ddd9f78474824f0c86b4a
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-04 16:25:03 +0900

    Added: connect db

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+require('dotenv').config();
+// async error
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+//middleware
+app.use(express.json());
+
+//rootes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a herf="/api/v1/products">products route</a>');
+});
+
+//product route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    //connect DB
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start()
