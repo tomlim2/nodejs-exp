1c6296870367af892ed8371d739c6a0804443fb4
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-04 23:42:53 +0900

    Added: query for name, company, and featured

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,29 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({featured:true});
-  res.status(200).json({ products, nbHits:products.length });
+  const search = 'aaa';
+  const products = await Product.find({
+    name: { $regex: search, $option: 'i' },
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { name: { $regex: name, $option: 'i' } };
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
