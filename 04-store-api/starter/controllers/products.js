12e7bb00b2d00b4b4e49acadeda0331d54ded1c1
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-04 21:57:44 +0900

    Added: basic filter functionality

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,14 +1,15 @@
-const getAllProductsStatic = async (req,res) => {
-  throw new Error('testing async error')
-  res.status(200).json({msg:'products testing route'})
-}
+const Product = require('../models/product');
 
-const getAllProducts = async (req,res) => {
-  res.status(200).json({msg:'products route'})
-}
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({featured:true});
+  res.status(200).json({ products, nbHits:products.length });
+};
+
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: 'products route' });
+};
 
 module.exports = {
   getAllProductsStatic,
   getAllProducts,
-
-}
+};
