44d5d2ed0ddcc739cfe20ee9a3feb90f74a2ff03
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-07 19:19:57 +0900

    Added: custom errors and refactor the errors

--- 05-JWT-Basics/starter/middleware/auth.js
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -1,22 +1,22 @@
 const jwt = require('jsonwebtoken');
-const CustomAPIError = require('../errors/custom-error');
+const { UnauthenticatedError } = require('../errors');
 
 const authenticationMiddleware = async (req, res, next) => {
-  const authHeader = req.headers.authorization
-  if(!authHeader || !authHeader.startsWith('Bearer ')){
-    throw new CustomAPIError('No token provided', 401)
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('No token provided');
   }
-  
-  const token = authHeader.split(' ')[1]
+
+  const token = authHeader.split(' ')[1];
   console.log(token);
 
-  try{
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    const {id, username} = decoded
-    req.user = {id, username}
-    next()
-  } catch(error){
-    throw new CustomAPIError('Not authrized to access this route', 401)
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('Not authrized to access this route');
   }
 };
 
