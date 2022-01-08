a0306b2ebbad929d02fbc1e274330f28fd436a91
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-08 16:04:10 +0900

    Refactor: remove next parameter since it updated

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,7 +1,5 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
-const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
