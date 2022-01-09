3bac9282262e49a0af3da95081b1af2e01f99e82
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-09 18:32:28 +0900

    Added: controller for getting single job based on user id

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -14,7 +14,7 @@ const login = async (req, res) => {
     throw new BadRequestError('Please provide email and password');
   }
   const user = await User.findOne({ email });
-  console.log(user)
+  // console.log(user)
   if (!user) {
     throw new UnauthenticatedError('Invalid Credentials');
   }
