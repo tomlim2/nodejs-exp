03410ae81071232ab4058956d04d97402235da13
Younsoo Tom Lim <tomandlim@gmail.com>
2022-01-11 19:15:24 +0900

    Added: Basic structure

---
+++ 07-file-upload/starter/controllers/productController.js
@@ -0,0 +1,12 @@
+const Product = require("../models/Product");
+const { StatusCodes } = require("http-status-codes");
+
+const createProduct = async (req, res) => {
+  res.send("create product");
+};
+
+const getAllProducts = async (req, res) => {
+  res.send("list of product");
+};
+
+module.exports = {createProduct, getAllProducts}
